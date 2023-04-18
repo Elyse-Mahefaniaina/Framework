91f0219ee0c01bb1f54e018d0b0fc8585703bb8e
Elyse-Mahefaniaina <119030086+Elyse-Mahefaniaina@users.noreply.github.com>
2023-04-18 06:33:06 +0300

    Sprint 3

--- Framework/src/util/Util.java
+++ Framework/src/util/Util.java
@@ -1,5 +1,9 @@
 package util;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Util {
 
     public String processUrl(String url_input, String ctx) {
@@ -8,6 +12,35 @@ public class Util {
         String url = url_input.substring(ctx_ind + ctx.length());
 
         return url;
+    }
+
+    public List<Class<?>> getAllClass(String path, String tomPath) throws ClassNotFoundException {
+        List<Class<?>> classes = new ArrayList<>();
+        File directory = new File(path);
+
+        if (!directory.exists()) {
+            return classes;
+        }
+
+        File[] files = directory.listFiles();
+        assert files != null;
+        for (File file : files) {
+            if (file.isDirectory()) {
+                List<Class<?>> innerClasses = this.getAllClass(file.getAbsolutePath(), tomPath);
+                classes.addAll(innerClasses);
+            } else if (file.getName().endsWith(".class")) {
+                String absolute_path_class = file.getPath().replace("\\", "/");
+                int tom_int_path = absolute_path_class.indexOf(tomPath);
+
+                String className = absolute_path_class.substring(tom_int_path + tomPath.length())
+                        .replace(".class", "")
+                        .replace("/", ".");
+                Class<?> clazz = Class.forName(className);
+
+                classes.add(clazz);
+            }
+        }
+        return classes;
     }
 
 }
