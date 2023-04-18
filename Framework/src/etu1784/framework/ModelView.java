aa068cbbc095a8144526695f0935c34471b5c166
Elyse-Mahefaniaina <119030086+Elyse-Mahefaniaina@users.noreply.github.com>
2023-04-18 06:40:08 +0300

    Sprint 5

---
+++ Framework/src/etu1784/framework/ModelView.java
@@ -0,0 +1,14 @@
+package etu1784.framework;
+
+import java.util.HashMap;
+
+public class ModelView {
+    private String view;
+
+    public String getView() {
+        return view;
+    }
+    public void setView(String view) {
+        this.view = view;
+    }
+}
